2506275847ca5d52f2e27cb2e271a95a14074cbe
kongyicn <kongyicn@163.com>
2019-04-17 17:09:59 +0800

    add test docker image

---
+++ artifacts/docker/bluedocker/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func SayHello(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Welcome to the Blue Docker!")
+	//fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", SayHello)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	handleRequests()
+}
